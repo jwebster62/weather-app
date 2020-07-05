57434d5ba2e9739899c42ddd0b8b81fcd690d23a
Joshua Webster <darkwolf62@gmail.com>
2020-07-05 13:19:53 -0500

    final

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
 const apiID = '2ec8b15d6b7487667d0362508db41ea4';
-const mainURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const mainURL = 'https://api.openweathermap.org/data/2.5/weather';
 const userZip = document.getElementById('zip');
 const userFeel = document.getElementById('feelings');
 const date = document.getElementById('date');
@@ -9,12 +9,53 @@ const content = document.getElementById('content');
 const server = 'http://localhost:3000'
     // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
 
 //Get the temp/weather
-const findWeather = async(mainURL, zip, apiID) => {
+const findWeather = async(mainURL, userZip, apiID) => {
     try {
-        const req = await fetch(`${mainURL}${userZip},us&units=imperial&APPID=${apiID}`, )
-        const results = await
+        const req = await fetch(`${mainURL}?zip=${userZip},us&units=imperial&APPID=${apiID}`, )
+        const results = await req.json()
+        const {
+            main: { temp },
+        } = results
+        return temp
+    } catch (e) {
+        throw e
     }
 }
+
+const postData = async(path, data) => {
+    try {
+        await fetch(path, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+    } catch (e) {
+        throw e
+    }
+}
+
+const updateUI = async(temps, newDate, feelings) => {
+    date.innerText = newDate
+    temp.innerText = `${temps}°F`
+    content.innerText = feelings
+}
+
+document.getElementById('generate').addEventListener('click', () => {
+    findWeather(mainURL, zip.value, apiID)
+        .then(temp => {
+            return { date: newDate, temp, content: feelings.value }
+        })
+        .then(data => {
+            postData('/add', data)
+            return data
+        })
+        .then(({ temp, date, content }) => updateUI(temp, date, content))
+        .catch(e => {
+            console.log(e)
+        })
+})
