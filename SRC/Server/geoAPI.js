5cce42e8044e793e9f28e5c4bfa1b395765ac98b
Joshua Webster <darkwolf62@gmail.com>
2020-08-11 21:26:05 -0500

    Created geoAPI function
    This is the function for getting the geo location data.

---
+++ SRC/Server/geoAPI.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+const fetchGeo = async(dest = '', key) => {
+    const geoURL = `http://api.geonames.org/search?username=${key}&type=json&name=`;
+
+    let res = await fetch(geoURL + dest);
+
+    if (res.ok) {
+        let geoData = await res.json();
+        if (geoData.geonames.length > 0) {
+            geoData = geoData.geonames[0];
+            return {
+                lat: geoData.lat.slice(0, 6),
+                lon: geoData.lng.slive(0, 6),
+                country: geoData.countryCode,
+                city: geoData.name
+            }
+        };
+    } else {
+        console.log(`ERROR: ${res.status} ${res.statusText}.`);
+    }
+    return {
+        lat: "no info",
+        lon: "no info",
+        country: "no info",
+        city: "no info"
+    };
+};
+
+module.exports = fetchGeo;
