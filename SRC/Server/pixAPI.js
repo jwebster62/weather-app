16f1693cf4994f0c44a6770ff97b5b68bcec81a1
Joshua Webster <darkwolf62@gmail.com>
2020-08-16 22:28:06 -0500

    Update pixAPI.js

--- SRC/Server/pixAPI.js
+++ SRC/Server/pixAPI.js
@@ -1,3 +1,7 @@
 const fetch = require('node-fetch');
 
-const getDestImg = async
+const getDestImg = async(city = '', country = '', pixKey) => {
+    let url = 'https://pixabay.com/api/?';
+    url = `${url}key=${pixKey}&q=${city}+${country}&category=travel`;
+
+}
