046a05ddb95919b2811056df56db0102aa398cfd
Joshua Webster <darkwolf62@gmail.com>
2020-08-16 22:41:12 -0500

    Pix
    -Pixabay js for getting destination images
    -Perplexed Penguin

--- SRC/Server/pixAPI.js
+++ SRC/Server/pixAPI.js
@@ -1,7 +1,20 @@
 const fetch = require('node-fetch');
+const { response } = require('express');
 
 const getDestImg = async(city = '', country = '', pixKey) => {
     let url = 'https://pixabay.com/api/?';
     url = `${url}key=${pixKey}&q=${city}+${country}&category=travel`;
+    let res = await fetch(url);
 
-}
+    if (res.ok) {
+        let pixData = await res.json();
+        if (pixData.hits.length > 0) {
+            return pixData.hits[0].webformatURL;
+        }
+    } else {
+        console.log(`Error: Can't find image` + response.status + response.statusText + '.');
+    }
+    return './src/client/media/penguin.png'
+};
+
+module.exports = getDestImg;
