39160f1f0e6cbe1ab889f05ad7be129657ef739f
Joshua Webster <darkwolf62@gmail.com>
2020-08-18 18:11:25 -0500

    changed all folders to lower case

--- SRC/tests/__formSubmit.test.js
+++ SRC/tests/__formSubmit.test.js
@@ -1,4 +1,4 @@
-import getTripInfo from '../Client/js/app';
+import getTripInfo from '../client/js/app';
 describe("Testing the submit functionality", () => {
     test("Testing the getTripInfo() function", () => {
         expect(getTripInfo).toBeDefined();
